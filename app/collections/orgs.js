76c37c7320dae3a3a26b2c3f7ae58368001b90de
Derek Lieu <dl@dereklieu.com>
2016-05-15 15:23:19 -0400

    Set defaults on org, correct url on orgs

--- app/collections/orgs.js
+++ app/collections/orgs.js
@@ -2,6 +2,8 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var Org = require('../models/org');
 var config = require('../config');
+var auth = require('../config');
+var util = require('../util');
 var cookie = require('../cookie');
 
 module.exports = Backbone.Collection.extend({
@@ -9,14 +11,27 @@ module.exports = Backbone.Collection.extend({
 
   initialize: function(models, options) {
     options = _.clone(options) || {};
-    _.bindAll(this);
-
     this.user = options.user;
+    this.api = util.getApiFlavor();
+    _.bindAll(this);
+  },
+
+  parse: function (res) {
+    if (this.api === 'gitlab') {
+      res.forEach(function (org) {
+        org.login = org.path;
+      });
+    }
+    return res;
   },
 
   url: function() {
     var token = cookie.get('oauth-token');
     var scope = cookie.get('scope');
+
+    if (this.api === 'gitlab') {
+      return auth.api + '/groups?access_token=' + token;
+    } // else GitHub
 
     // If not authenticated, show public repos for user in path.
     // https://developer.github.com/v3/orgs/#list-user-organizations
